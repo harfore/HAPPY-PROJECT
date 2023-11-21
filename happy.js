204f586667a5533266a9fe537c96595926f1bcaa
harfore <remiharfoush@gmail.com>
2023-11-21 21:00:21 +0100

    .

--- happy.js
+++ happy.js
@@ -68,7 +68,7 @@ const myQuestions = [
 const buildQuiz = () => {
     const output = [];
     myQuestions.forEach((currentQuestion, questionNumber) => {
-        answers = [];
+        /** const answer = []; !!! */
         for (letter in currentQuestion.answers) {
             answers.push(`<label> <input type="radio" name="question${questionNumber}" value="${letter}"> ${letter} : ${currentQuestion.answers[letter]} </label>`);
         }
@@ -77,9 +77,14 @@ const buildQuiz = () => {
     quizContainer.innerHTML = output.join('');
 };
 
+function buildQuiz() { // variable to store the HTML output const output = []; // for each question... myQuestions.forEach( (currentQuestion, questionNumber) => { // variable to store the list of possible answers const answers = []; // and for each available answer... for(letter in currentQuestion.answers){ // ...add an HTML radio button answers.push( `<label> <input type="radio" name="question${questionNumber}" value="${letter}"> ${letter} : ${currentQuestion.answers[letter]} </label>` ); } // add this question and its answers to the output output.push( `<div class="question"> ${currentQuestion.question} </div> <div class="answers"> ${answers.join('')} </div>` ); } ); // finally combine our output list into one string of HTML and put it on the page quizContainer.innerHTML = output.join('');
+}
+
+
 const showResults = () => { // display quiz right away
 
 }
+
 buildQuiz(); // on submit, show results
 
 submitButton.addEventListener('click', showResults);
